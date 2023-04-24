f2576b2517b41b7ee27c7ffd50619f30791498c6
wugi <17031278@itcelaya.edu.mx>
2023-04-23 20:41:17 -0600

    ver 1.0

--- src/components/Mielesese.jsx
+++ src/components/Mielesese.jsx
@@ -1,11 +1,16 @@
 import React from "react"
-import MielesesePNG from "./../public/mielesese.png"
+import MielesesePNG from "./../../public/mielesese.png"
 
-export default function ()
+export default function Mielesese(props)
 {
+   // const top = -50 * props.id + Math.floor(Math.random() * 320) + 50 + "px";
+   // const left = (Math.floor(Math.random() * -500) - 50) + "px";
+
+   // const movement = "movement " + ((Math.random() * 5) + 2) + 's  linear 1';
+
    return (
-      <div>
-         <img src={MielesesePNG}></img>
+      <div style={{animation: props.movement}} className="mielesese-container mel">
+         <img style={{top: props.top, left: props.left}} className="mielesese" src={MielesesePNG}></img>
       </div>
    );
 }
